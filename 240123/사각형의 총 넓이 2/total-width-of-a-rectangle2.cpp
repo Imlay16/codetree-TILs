2d62e70a14111e28d8b4dd4a49cb507f56250906
Imlay16 <100759623+Imlay16@users.noreply.github.com>
2024-01-23 16:27:57 +0900

    Title: 사각형의 총 넓이 2, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240123/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_N   200
+#define OFF_SET 100
+
+int cord[MAX_N][MAX_N];
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    int rectangle = 0;
+
+    int x1, y1, x2, y2;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        for (int j = x1; j < x2; j++) {
+            for (int k = y1; k < y2; k++) {
+                cord[j][k] = 1;
+            }
+        }
+    }
+
+    for (int i = 0; i < MAX_N; i++) {
+        for (int j = 0; j < MAX_N; j++) {
+            if (cord[i][j] == 1) rectangle++;
+        }
+    }
+    
+    cout << rectangle;
+    return 0;
+}
