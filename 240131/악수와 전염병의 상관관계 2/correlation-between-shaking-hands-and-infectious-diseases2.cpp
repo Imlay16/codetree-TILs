857b52e4cad56574f7034d4576330713e3e630c4
Imlay16 <100759623+Imlay16@users.noreply.github.com>
2024-01-31 22:00:05 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240131/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240131/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 #define TIME 250
 int hand_shake[TIME][2];
-int contact[100];
-int infected[100];
 
 int main() {
     
@@ -13,38 +11,44 @@ int main() {
 
     cin >> N >> K >> P >> T;
 
+    int contact[N] = { 0, };
+    int infected[N] = { 0, };
+
+    infected[P - 1] = 1;
+    contact[P - 1] = K;
+
+    int time = 0;
+
     for (int i = 0; i < T; i++) {
         cin >> t >> x >> y;
+
+        if (time < t) time = t;
 
         hand_shake[t][0] = x;
         hand_shake[t][1] = y;
     }
 
-    
-    contact[P] = K;
+    for (int i = 0; i < time; i++) {
 
-    for (int i = 0; i < TIME; i++) {
+        int index1 = hand_shake[i][0] - 1;
+        int index2 = hand_shake[i][1] - 1;
 
-        if (hand_shake[i][0] == P) {
-            
-            if (contact[i] > 0) {
-
-                infected[hand_shake[i][1]] = 1;
-                P = hand_shake[i][1];
-            }
+        if (infected[index1] && contact[index1]) {
+            infected[index2] = 1;
+            contact[index2] = K;
+            contact[index1]--;
         }
-        else if (hand_shake[i][1] == P) {
-
-            if (contact[i] > 0) {
-
-                infected[hand_shake[i][0]] = 1;
-                P = hand_shake[i][0];
-            }
+        else if (infected[index2] && contact[index2]) {
+            infected[index1] = 1;
+            contact[index1] = K;
+            contact[index2]--;
         }
-        
+        else {
+
+        }
     }
 
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < N; i++) {
         cout << infected[i];
     }
 
