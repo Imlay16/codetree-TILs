f1c30e00c0454db759e6d00cd72c1e330fb42962
Imlay16 <100759623+Imlay16@users.noreply.github.com>
2024-02-06 21:06:08 +0900

    Title: 방향에 맞춰 이동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240206/방향에 맞춰 이동/move-in-direction.cpp
+++ 240206/방향에 맞춰 이동/move-in-direction.cpp
@@ -12,26 +12,25 @@ int main() {
 
     char direction;
     int distance;
+    int dir;
 
     for (int i = 0; i < n; i++) {
         cin >> direction >> distance;
 
         if (direction == 'E') {
-            x += distance * dx[0];
-            y += distance * dy[0];
+            dir = 0;
         }
         else if (direction == 'S') {
-            x += distance * dx[1];
-            y += distance * dy[1];
+            dir = 1;
         }
         else if (direction == 'W') {
-            x += distance * dx[2];
-            y += distance * dy[2];
+            dir = 2;
         }
         else if (direction == 'N') {
-            x += distance * dx[3];
-            y += distance * dy[3];
+            dir = 3;
         }
+        x += distance * dx[dir];
+        y += distance * dy[dir];
     }
 
     cout << x << " " << y;
