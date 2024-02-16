a602560d8e513d41b1fe7573c38414b4083fcc56
Imlay16 <100759623+Imlay16@users.noreply.github.com>
2024-02-16 23:29:10 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240216/빙빙 돌며 사각형 채우기/snail-alphabet-square.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int n, m;
+
+bool InRange(int x, int y) {
+    return (0 <= x && x < n && 0 <= y && y < m);
+}
+
+int main() {
+
+    int x = 0, y = 0;
+    cin >> n >> m;
+
+    char grid[n][m];
+    memset(grid, 0, n*m);
+
+    int dir_num = 0;
+    int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+
+    char cnt = 65;
+    grid[x][y] = cnt++;
+
+    for (int i = 1; i < n*m; i++) {
+
+        int nx = x + dx[dir_num], ny = y + dy[dir_num];
+
+        if (InRange(nx, ny) && grid[nx][ny] == 0) {
+            x = x + dx[dir_num], y = y + dy[dir_num];
+            grid[x][y] = cnt;
+        }
+        else {
+            dir_num = (dir_num + 1) % 4;
+            x = x + dx[dir_num], y = y + dy[dir_num];
+            grid[x][y] = cnt;
+        }
+        cnt++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
