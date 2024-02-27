fa4169624e940b29437319c239b74e6a92d415d5
Imlay16 <100759623+Imlay16@users.noreply.github.com>
2024-02-27 21:15:07 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240227/이상한 진수 2/awkward-digits-2.cpp
+++ 240227/이상한 진수 2/awkward-digits-2.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <string>
+
 using namespace std;
 
-
-int main() {
-
-    string num;
-    cin >> num;
-
-    for (int i = 0; i < num.length(); i++) {
+int changeNum(string num, int flag) {
+    for (int i = 0; i < num.length() - flag; i++) {
         if (num[i] == '0') {
             num[i] = '1';
             break;
         }
     }
-    
+
     int len = num.length();
 
     int total = 0;
@@ -27,8 +24,30 @@ int main() {
         }
         change *= 2;
     }
+    return total;
+}
 
-    cout << total;
+int main() {
+
+    string num;
+    cin >> num;
+
+    int flag = 0;
+
+    int len = num.length();
+
+    if (len == 1) {
+        if (num[0] == '0') cout << 1;
+        else cout << 0;
+    }
+    else {
+        if (num.find('0')) {
+            num[len - 1] = '0';
+            flag = 1;
+        }
+        int number = changeNum(num, flag);
+        cout << number;
+    }
 
     return 0;
 }
