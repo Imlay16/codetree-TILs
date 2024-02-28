e46c6d58d3f5c8779c3f864a524731b75cbcc7a7
Imlay16 <100759623+Imlay16@users.noreply.github.com>
2024-02-28 21:11:08 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240228/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+#define MAX_N 2000
+#define OFF_SET 1000
+
+int x[MAX_N], y[MAX_N];
+int dis[100];
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+
+        x[i] += OFF_SET;
+        y[i] += OFF_SET;
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        int distance = abs(x[i] - x[i + 1]) + abs(y[i] - y[i + 1]);
+        dis[i] = distance;
+    }
+
+    int min = INT_MAX;
+    int total = 0;
+
+    for (int i = 0; i < n; i++) {
+        total += dis[i];
+    }
+
+    for (int i = 1; i < n - 1; i++) {
+        total -= dis[i];
+
+        if (min > total) min = total;
+        
+        total += dis[i];
+    }
+
+    cout << min + 1;
+    return 0;
+}
