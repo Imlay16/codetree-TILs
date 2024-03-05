f8492d30a8ba4ba41f0a5102f09d132dddab9be9
Imlay16 <100759623+Imlay16@users.noreply.github.com>
2024-03-05 21:41:36 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
+++ 240305/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -17,21 +17,14 @@ int main() {
         cin >> num[i];
     }
 
+
     for (int i = 0; i < n-1; i++) {
 
-        int prev_index = i - 1;
-        int next_index = i + 1;
+        for (int j = i + 2; j < n; j++) {
 
-        total = num[i];
-
-        for (int j = 0; j < n; j++) {
-
-            if ((i != 0 && j == prev_index) || j == next_index || j == i) continue;
-
-            total += num[j];
-
-            if (max < total) max = total;
-            total -= num[j];
+            if (max < num[i] + num[j]) {
+                max = num[i] + num[j];
+            }
         }
     }
 
